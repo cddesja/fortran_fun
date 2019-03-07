8160b0ee31bd42889c1653dc230ff7f670e1057d
Chris Desjardins <cddesjardins@gmail.com>
2019-03-07 15:13:57 -0600

    some example fortran code

---
+++ ex4_5_1.f95
@@ -0,0 +1,15 @@
+! Example Program
+
+program example1
+
+	implicit none
+	integer :: radius, diameter
+	integer :: height = 100, width = 150
+	real :: area, perimeter
+	real :: length = 123.5, distance = 413.761
+	real, parameter :: pi = 3.14159
+	character(13) :: msg = "Hello, Chris."
+
+	write (*, *) "Greeting: ", msg
+
+end program example1
---
+++ hw.f95
@@ -0,0 +1,7 @@
+! Simple Example Program
+program first
+
+	write (*, *) "Hello World"
+	write (*, *) "My name is Chris!"
+
+end program first
